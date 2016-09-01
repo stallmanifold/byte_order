928cb00b0d0c43ea2560f8647d67d95fc108871a
LambdaXymox <lambda.xymox@gmail.com>
2016-09-01 17:54:33 -0500

    Add documentation.

--- src/byte_order.rs
+++ src/byte_order.rs
@@ -1,12 +1,13 @@
 use std::fmt;
 use std::mem;
 
+
+/// The `ByteOrder` enum records the possible byte orders a machine can have.
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum ByteOrder {
     BigEndian,
     LittleEndian,
 }
-
 
 impl fmt::Display for ByteOrder {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt:: Result {
@@ -17,7 +18,22 @@ impl fmt::Display for ByteOrder {
     }
 }
 
-
+/// The function `byte_order` calculates the system's byte order.
+///
+/// # Examples
+///
+/// Calculate a system's byte order:
+///
+/// ```rust
+///
+/// use byte_order::{ByteOrder};
+///
+/// fn main() {
+///     let byte_order = byte_order::byte_order();
+///
+///     println!("This machine has byte order: {}", byte_order);
+/// }
+/// ```
 #[allow(unused_assignments)]
 pub fn byte_order() -> ByteOrder {
     let mut arr: [u8; 2] = [0 as u8,0 as u8];
@@ -31,7 +47,7 @@ pub fn byte_order() -> ByteOrder {
 
 }
 
-
+/// Decides whether a system has big endian byte order.
 pub fn is_big_endian() -> bool {
     match byte_order() {
         ByteOrder::BigEndian    => true,
@@ -39,11 +55,10 @@ pub fn is_big_endian() -> bool {
     }
 }
 
-
+/// Decides whether a system has big endian byte order.
 pub fn is_little_endian() -> bool {
     match byte_order() {
         ByteOrder::BigEndian    => false,
         ByteOrder::LittleEndian => true,
     }
 }
-
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+/*!
+This crate is a library that provides methods for determining the byte order of the system a rust
+program is running on.
+*/
+#![crate_name="byte_order"]
+pub use byte_order::ByteOrder;
+pub use byte_order::{byte_order, is_big_endian, is_little_endian};
+
 pub mod byte_order;
 
 #[cfg(test)]
